8a0c3b6efa1a02a43b432d9cfa7d265d9513b333
rhinolary <39052241+Roguim@users.noreply.github.com>
2024-04-23 10:52:49 +1000

    Test custom onLoad handler; found out to be server-side only; preserving the code in git history just in case.

--- src/main/java/io/github/roguim/WynnAlmanac.java
+++ src/main/java/io/github/roguim/WynnAlmanac.java
@@ -1,8 +1,11 @@
 package io.github.roguim;
 
 import io.github.roguim.interfaces.LivingEntityDeathCallback;
+import io.github.roguim.interfaces.LoadEntityCallback;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
+import net.minecraft.entity.Entity;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
@@ -18,8 +21,30 @@ public class WynnAlmanac implements ModInitializer {
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
 
+		/*ClientEntityEvents.ENTITY_UNLOAD.register((entity1, world) -> {
+			Text ename = entity1.getCustomName();
+			if(ename != null) {
+				String enamed = ename.getString();
+				LOGGER.info(enamed);
+			}
+		});*/
+
+		LoadEntityCallback.EVENT.register((entity,world) -> {
+			LOGGER.info("[WynnAlmanac] Event Triggered");
+			Text name = entity.getCustomName();
+			if(name != null) {
+				String named = name.getString();
+				LOGGER.info(named);
+			}
+
+            return ActionResult.PASS;
+        });
+
 		LivingEntityDeathCallback.EVENT.register(((entity, reason) -> {
 			Text name = entity.getCustomName();
+
+
+
 			if(name != null) {
 				String named = name.getString();
 				Pattern matchPattern = Pattern.compile("§c[A-Za-z0-9? §]+\\[Lv. (([0-9])|(\\?))+]");
@@ -27,6 +52,17 @@ public class WynnAlmanac implements ModInitializer {
 				if (matcher.find()) {
 					LOGGER.info(named);
 					LOGGER.info(reason.getName());
+					Entity attacker = reason.getAttacker();
+					if (attacker != null) {
+						LOGGER.info(attacker.getEntityName());
+					} else {
+						LOGGER.warn("No attacker found for " + named);
+					}
+					Entity attacker2 = entity.getLastAttacker();
+
+					if(attacker2 != null) {
+						LOGGER.info(attacker2.getEntityName());
+					}
 				}
 			}
 
