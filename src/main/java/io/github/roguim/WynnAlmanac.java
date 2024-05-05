9b18f837b98080a5fd15263e84510fae409946d9
rhinolary <39052241+Roguim@users.noreply.github.com>
2024-05-05 17:04:31 +1000

    Started thinking of a *new* way to detect what player killed it, and found a quirk with the way wynn works - YAY!

--- src/main/java/io/github/roguim/WynnAlmanac.java
+++ src/main/java/io/github/roguim/WynnAlmanac.java
@@ -3,12 +3,19 @@ package io.github.roguim;
 import io.github.roguim.interfaces.LivingEntityDeathCallback;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.Box;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,31 +24,30 @@ public class WynnAlmanac implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("[WynnAlmanac] Booting up...");
 
 		LivingEntityDeathCallback.EVENT.register(((entity, reason) -> {
 			Text name = entity.getCustomName();
 
-
+			LOGGER.info("[WynnAlmanac] Event triggered.");
 
 			if(name != null) {
+				LOGGER.info("[WynnAlmanac] Name exists!");
 				String named = name.getString();
-				Pattern matchPattern = Pattern.compile("§c[A-Za-z0-9? §]+\\[Lv. (([0-9])|(\\?))+]");
+				// TODO Also put this in a config
+				Pattern matchPattern = Pattern.compile("§[ace][A-Za-z0-9? §]+\\[Lv. (([0-9])|(\\?))+]");
 				Matcher matcher = matchPattern.matcher(named);
+				// TODO Put the area / delay into a config file so this can be changed without rebooting, this is too damn painful.
+				List<Entity> entityList = entity.getWorld().getOtherEntities(entity, Box.of(entity.getPos(),2,2,2));
 				if (matcher.find()) {
-					LOGGER.info(named);
-					LOGGER.info(reason.getName());
-					Entity attacker = reason.getAttacker();
-					if (attacker != null) {
-						LOGGER.info(attacker.getEntityName());
-					} else {
-						LOGGER.warn("No attacker found for " + named);
-					}
-					Entity attacker2 = entity.getLastAttacker();
-
-					if(attacker2 != null) {
-						LOGGER.info(attacker2.getEntityName());
-					}
+					LOGGER.info("[WynnAlmanac] Match found: " + named);
+					LOGGER.info("[WynnAlmanac] Other entity count: " + entityList.size());
+				} else {
+					// THIS CASE HAPPENS MORE OFTEN THAN NOT, DUE TO WYNN RENAMING THE BASE MOB TO THE DAMAGE TAKEN
+					// Either needs a tick or two of delay before running the code, or a larger area.
+					// Once delayed, then check again.
+					LOGGER.warn("[WynnAlmanac] " + named);
+					LOGGER.info("[WynnAlmanac] Other entity count: " + entityList.size());
 				}
 			}
 
